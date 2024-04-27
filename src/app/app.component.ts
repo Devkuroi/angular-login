1f66348128273ae8e38e6ed59b100b1e6bcbe01f
kuroi <joan2000oviedo@gmail.com>
2024-04-27 14:04:50 -0500

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, DashboardComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'angular-login';
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [MatTabsModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
